f89f661b2cb5db294872907aed8625a47fd3a7a6
Irfan Alam <108110097+Irfanlam123@users.noreply.github.com>
2025-02-13 14:54:43 +0530

    fix user

--- models/User.js
+++ models/User.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: Number,required:true},
-    password: { type: Number||String ,required:true},
+    email: { type: String, required: true, unique: true }, // Add unique constraint
+    phone: { type: String, required: true, unique: true }, // Phone as string, also unique
+    password: { type: String, required: true }, // Password as string
   },
-)
+  { timestamps: true } // This will add createdAt and updatedAt fields automatically
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
